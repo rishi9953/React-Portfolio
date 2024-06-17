ac80d31f0fe2f5f3911b7901e5fe2cd8748775dd
rishi9953 <rk5680179@gmail.com>
2024-06-17 16:14:48 +0530

    setup gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import Header from './Header/header';
 import AboutMe from './aboutMe/aboutMe';
-import MyServices from './Services/services';
-import MyExperience from './experience/experience';
-import MySkills from './Skills/skills';
-import ContactUs from './ContactUs/contactUs';
+// import MyServices from './Services/services';
+// import MyExperience from './experience/experience';
+// import MySkills from './Skills/skills';
+// import ContactUs from './ContactUs/contactUs';
 import './App.css';
-import MyFooter from './Footer/footer';
+// import MyFooter from './Footer/footer';
 
 function App() {
   return (
     <>
       <Header></Header>
       <AboutMe></AboutMe>
-      <MyServices></MyServices>
+      {/* <MyServices></MyServices>
       <MyExperience></MyExperience>
       <MySkills></MySkills>
       <ContactUs></ContactUs>
-      <MyFooter></MyFooter>
+      <MyFooter></MyFooter> */}
     </>
 
 
--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import './header.css';
 
 function scrollToTarget(id) {
@@ -7,33 +8,86 @@ function scrollToTarget(id) {
 }
 
 function Header() {
+    const navRef = useRef();
+
+    const showNavBar = () => {
+        navRef.current.classList.toggle(
+            "responsive_nav"
+        );
+    }
+    const [menuOpen, setMenuOpen] = useState(false);
     return (<>
-        <div className="header">
-            <ul>
+        <nav>
+            <a href="" className='title'>About  <span style={{ color: 'yellow' }}>Me</span></a>
+            <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ''}>
                 <li onClick={(() => {
-                    scrollToTarget('footer');
+                    scrollToTarget('hom');
                 })}>
-                    <a id='about'>ABOUT</a>
-                </li>
-                <li onClick={(() => {
-                    scrollToTarget('cont');
-                })}>
-                    <a>CONTACT</a>
-                </li>
-                <li onClick={(() => {
-                    scrollToTarget('exp');
-                })}>
-                    <a>EXPERIENCES</a>
+                    <a href='#hom'>HOME</a>
                 </li>
                 <li onClick={(() => {
                     scrollToTarget('ser');
                 })}>
-                    <a id='service'>SERVICES</a>
+                    <a href='#ser' id='service'>SERVICES</a>
+                </li>
+                <li onClick={(() => {
+                    scrollToTarget('exp');
+                })}>
+                    <a href='#exp'>EXPERIENCES</a>
+                </li>
+                <li onClick={(() => {
+                    scrollToTarget('cont');
+                })}>
+                    <a href='#cont'>CONTACT</a>
+                </li>
+                <li onClick={(() => {
+                    scrollToTarget('footer');
+                })}>
+                    <a href='#footer' id='about'>ABOUT</a>
+                </li>
+
+
+
+
+            </ul>
+        </nav>
+
+        {/* <div className="header">
+            <ul>
+
+
+                <li onClick={(() => {
+                    scrollToTarget('footer');
+                })}>
+                    <a href='#footer' id='about'>ABOUT</a>
+                </li>
+                <li onClick={(() => {
+                    scrollToTarget('cont');
+                })}>
+                    <a href='#cont'>CONTACT</a>
+                </li>
+                <li onClick={(() => {
+                    scrollToTarget('exp');
+                })}>
+                    <a href='#exp'>EXPERIENCES</a>
+                </li>
+                <li onClick={(() => {
+                    scrollToTarget('ser');
+                })}>
+                    <a href='#ser' id='service'>SERVICES</a>
                 </li>
                 <li onClick={(() => {
                     scrollToTarget('hom');
                 })}>
-                    <a>HOME</a>
+                    <a href='#hom'>HOME</a>
+                </li>
+                <li>
+                    <FaBars className="nav-btn" ></FaBars>
                 </li>
                 <li className='about' onClick={(() => {
                     scrollToTarget('hom');
@@ -45,6 +99,7 @@ function Header() {
                 </li>
             </ul>
         </div>
+     */}
     </>)
 }
 
